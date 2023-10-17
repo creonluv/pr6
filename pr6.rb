ed5984f1f060d30c6554a2f87fae38ed9419e717
creonluv <tixehack@gmail.com>
2023-10-17 22:02:55 +0300

    commit1

---
+++ pr6.rb
@@ -0,0 +1,25 @@
+def valid_ipv4?(str)
+  parts = str.split(".")
+
+  return false unless parts.length == 4
+
+  parts.each do |part|
+    return false unless part.match?(/^\d+$/)
+    num = part.to_i
+    return false unless (0..255).cover?(num)
+  end
+
+  return true
+end
+
+loop do
+  print "Введіть IPv4-адресу: "
+  address = gets.chomp
+
+  if valid_ipv4?(address)
+    puts "Це коректна IPv4-адреса."
+  else
+    puts "Це не коректна IPv4-адреса."
+  end
+end
+
